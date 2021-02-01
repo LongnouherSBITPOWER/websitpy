7475f47fe559879e67817ec29dfd53a1611d3591
ລຸງນູເຮີ SB IT POWER ສອນອອນໄລນ໌ <75841785+LongnouherSBITPOWER@users.noreply.github.com>
2021-02-01 15:36:11 +0700

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,16 @@
+#urls.py
+from django.urls import path, include 
+from .views import *
+
+urlpatterns = [
+    path('', Home, name='home-page'),
+    path('about/',About, name='about-page'),
+    path('contact/',Contact, name='contact-page'),
+    path('addproduct/', AddProduct, name='addproduct-page'),
+    path('allproduct/',Product, name='allproduct-page'),
+    path('register/',Register, name='register-page'),
+    path('addtocart/<int:pid>/',AddtoCart,name='addtocart-page'),
+    path('mycart/',MyCart,name='mycart-page'),
+    path('mycart/edit/',MyCartEdit,name='mycartedit-page'),
+    path('checkout/',Checkout,name='checkout-page'),
+]
